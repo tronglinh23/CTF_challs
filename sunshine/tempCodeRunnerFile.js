fbc16a12fdd7397ec7876b32e083d3cef1d1342d
xon1l <93851819+tronglinh23@users.noreply.github.com>
2023-10-09 22:37:12 +0700

    Change sunshineCTF2023 FILE

--- sunshine/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-global_limit = 30
-// for(let i = 0; i < global_limit; i++) {
-//     setTimeout(() => {
-//         var y = document.getElementById(frequencies[i]);
-//         y.click();
-//     }, 1000);
-// }
--- sunshine/test.js
+++
@@ -1,4 +0,0 @@
-frequencies = ["123","123"]
-// console.log(JSON.parse(frequencies))
-console.log(JSON.stringify(frequencies))
-// console.log(typeof(JSON.dumps(frequencies)))
